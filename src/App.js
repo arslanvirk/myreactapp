9c94818f5d4d0d661653f38d0e97bf581a0ebb61
Arslan Ameen <arslan.virk11@gmail.com>
2020-06-07 00:06:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br/> Updated by Arslan Ameen
         <a
           className="App-link"
           href="https://reactjs.org"
